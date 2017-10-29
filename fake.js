efdfef3c1c51c2ea54cd542bf77136b9f648e32f
Chris Valcke <valckec@gmail.com>
2017-10-29 12:52:55 -0700

    add function in new feature
    create add function for new feature brach

--- fake.js
+++ fake.js
@@ -8,3 +8,10 @@ function nothing() {
 nothing();
 
 // see if change is picked up
+
+//we're adding a new section for the feature branch
+var b = function add(a1, a2) {
+    return a1+a2;
+}
+
+b(2,5);
